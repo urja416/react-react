ab352017643c0cf0050e13e9cee688c4b6fe9175
urja416 <urjadhungana008@gmail.com>
2024-02-25 17:59:58 +0545

    change

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,18 @@
 import React from 'react'
-import CardDisplay from '../components/CardDisplay'
+
 import UserForm from '../components/UserForm';
 
+
 const HomePage = () => {
+  setTimeout(() => {
+    console.log('hello jee');
+  }, 4000);
 
   // const image = 'https://images.unsplash.com/photo-1706391989349-7882ece26c0c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHx8';
   // const image1 = 'https://images.unsplash.com/photo-1682686581854-5e71f58e7e3f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHx8';
   // const title = 'title1'
   // const detail = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa quos quam cum quis praesentium quidem nihil excepturi itaque atque voluptatem.'
+
 
 
   return (
@@ -23,6 +28,7 @@ const HomePage = () => {
 
 
       <UserForm />
+
 
     </div>
   )
